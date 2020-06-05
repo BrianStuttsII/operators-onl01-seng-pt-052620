6bc77a151601aa3734415e1706c02a6f9d3350d5
reflective-refactoring-7309 <bristuttii@gmail.com>
2020-06-05 17:59:08 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
-def unsafe?(speed)
+
+def unsafe?(speed= 50)
+    if speed < 40 || speed > 60
+    "true"
+    else
+      "false"
+    end
+  
+  end
+    
+
+	def not_safe?(speed= 50)
+ 
+ speed < 40 || speed > 60 ? true : false
 
 end
-
-
-
-def not_safe?(speed)
-	
-end
-	
-
-
